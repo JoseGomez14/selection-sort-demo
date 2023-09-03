8d61ca963332977729dad74d2f14a8449e494d81
JoseGomez14 <jose.gomez14@udea.edu.co>
2023-09-03 14:09:53 -0500

    chore: :zap: Update data type schemas

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,15 @@
 type TMotel = {
+  id: number;
   name: string;
   rate: number;
   mean_price: number;
   opening_date: string;
+  image?: string;
+};
+
+type TData = {
+  name: string;
+  motelId: number;
+  openTime: number;
+  clicks: number;
 };
